0554d6c7f1844eba2cc4fce32fb859c96c9898bd
Valery Abmetka <valeryabmetkawork@gmail.com>
2024-02-24 11:07:12 +0300

    feat: added prettier

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
-import { createBrowserRouter } from "react-router-dom"
-import { Main } from "./pages/main/index.ts"
-import App from "./App"
+import { createBrowserRouter } from "react-router-dom";
+import { Main } from "./pages/main/index.ts";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
@@ -8,5 +8,5 @@ const router = createBrowserRouter([
     element: <App />,
     children: [{ path: `/`, element: <Main /> }],
   },
-])
-export default router
+]);
+export default router;
