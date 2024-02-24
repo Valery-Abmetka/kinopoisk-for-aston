f1e8c76ca284f2c7f72a34edae6277bf2af54631
Valery Abmetka <valeryabmetkawork@gmail.com>
2024-02-24 13:09:31 +0300

    feat: added CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
   ],
   overrides: [
     {
@@ -21,15 +23,24 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "import/prefer-default-export": "off",
     "react/jsx-no-useless-fragment": "warn",
     "no-console": "warn",
+  },
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect",
+    },
   },
 };
