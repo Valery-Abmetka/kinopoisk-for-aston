e7a2582e1980156fa3a5c64d03497b15d61f3842
Valery Abmetka <valeryabmetkawork@gmail.com>
2024-02-24 10:23:52 +0300

    feat: install react-router-dom

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom"
+import { Main } from "./pages/main/index.ts"
+import App from "./App"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: `/`, element: <Main /> }],
+  },
+])
+export default router
