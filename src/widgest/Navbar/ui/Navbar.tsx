b97139fdce7548f46672393d8fd8d3ee44135b25
Valery Abmetka <valeryabmetkawork@gmail.com>
2024-02-25 09:43:37 +0300

    feat: added Navbar

--- src/widgest/Navbar/ui/Navbar.tsx
+++ src/widgest/Navbar/ui/Navbar.tsx
@@ -1,3 +1,70 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import cn from "classnames";
+import { MdLocalMovies as HomeIcon } from "react-icons/md";
+
 export function Navbar() {
-  return <h2>navbar</h2>;
+  return (
+    <nav className={styles.navBar}>
+      <NavLink
+        className={({ isActive }) =>
+          cn(styles.link, {
+            [styles.active]: isActive,
+          })
+        }
+        to="/"
+      >
+        <HomeIcon className={styles.icon} />
+      </NavLink>
+      <div className={styles.navBarLinks}>
+        <NavLink
+          className={({ isActive }) =>
+            cn(styles.link, {
+              [styles.active]: isActive,
+            })
+          }
+          to="/signin"
+        >
+          Войти
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            cn(styles.link, {
+              [styles.active]: isActive,
+            })
+          }
+          to="/signup"
+        >
+          Зарегистрироваться
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
+//добавить когда зашел Пользователь
+//  <NavLink
+//     className={({ isActive }) =>
+//       cn(
+//         styles.link,
+//         {
+//           [styles.active]: isActive,
+//         },
+//       )
+//     }
+//     to="/favorites"
+//   >
+//     Favorites
+//   </NavLink>
+//   <NavLink
+//     className={({ isActive }) =>
+//       cn(
+//         styles.link,
+//         {
+//           [styles.active]: isActive,
+//         },
+//       )
+//     }
+//     to="/history"
+//   >
+//     History
+//   </NavLink>
