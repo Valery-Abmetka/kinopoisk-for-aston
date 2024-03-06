fe05914f1f5aca9ec098817bf74d2354325b2707
Valery Abmetka <valeryabmetkawork@gmail.com>
2024-03-07 01:34:31 +0300

    feat: add ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,23 +2,27 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "../widgest/Navbar";
 import { useSelector } from "react-redux";
-
 import { useAuthCheck } from "../shared";
 import { getIsLoadingAuth } from "../shared/reducers/Authorization";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   useAuthCheck();
   const isLoadingCheckAuth = useSelector(getIsLoadingAuth);
-
-  return isLoadingCheckAuth ? (
-    <h1>Загрузка</h1>
-  ) : (
-    <div className="app">
-      <Navbar />
-      <div>
-        <Outlet />
+  if (isLoadingCheckAuth) {
+    <h1>Загрузка</h1>;
+  }
+  return (
+    <ErrorBoundary
+      fallback={<div>Мы уже знаем о проблеме и стараемся ее решить</div>}
+    >
+      <div className="app">
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
